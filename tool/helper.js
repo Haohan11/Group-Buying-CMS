1544fe083a97856b728d671a55e85bf8b1d6ff95
haohan <han.chang@i2csolution.com>
2024-07-19 18:53:23 +0800

    add addpadding helper function

--- tool/helper.js
+++ tool/helper.js
@@ -47,10 +47,33 @@ export const checkArray = (arr) => Array.isArray(arr) && arr.length > 0;
 
 export const toArray = (target) => (Array.isArray(target) ? target : [target]);
 
+export const getAddPadding = (() => {
+  const checkType = (target) => {
+    const type = typeof target;
+    if (type === "string") return true;
+    throw Error(`addPadding only accept string input but get "${type}" type.`);
+  };
+
+  return (symbol) => {
+    checkType(symbol);
+
+    return (str, digits) => {
+      checkType(str);
+      while (str.length < digits) str = symbol + str;
+      return str;
+    };
+  };
+})();
+
+export const addZeroPadding = getAddPadding("0");
+
 export const getCurrentTime = () => {
   const date = new Date();
+  const month = addZeroPadding(`${date.getMonth() + 1}`, 2);
+  const day = addZeroPadding(`${date.getDate()}`, 2);
+  const hour = addZeroPadding(`${date.getHours()}`, 2);
+  const minute = addZeroPadding(`${date.getMinutes()}`, 2);
+  const second = addZeroPadding(`${date.getSeconds()}`, 2);
 
-  return `${date.getFullYear()}-${
-    date.getMonth() + 1
-  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  return `${date.getFullYear()}-${month}-${day} ${hour}:${minute}:${second}`;
 };
