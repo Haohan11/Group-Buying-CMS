03cd1bc121221216443f53d4d29676c29e38c129
haohan <han.chang@i2csolution.com>
2024-08-07 15:38:21 +0800

    handle alert modal show when change member

--- data-list/edit-modal/components/SaleMember.js
+++ data-list/edit-modal/components/SaleMember.js
@@ -16,36 +16,51 @@ const SaleMember = (props) => {
     <>
       {getInput("select")({
         ...props,
+        value: (() => {
+          if (!hoistPreLoadData.get()?.[props.name]) return null;
+          const member = hoistPreLoadData.get()[props.name]?.[0];
+          const memberId = hoistFormik.get().values?.[props.name];
+          return hoistPreLoadData
+            .get()
+            [props.name].find(
+              (option) => option.value === (memberId ? memberId : member?.id)
+            );
+        })(),
         isDisabled: hoistFormik.get().values?._separate,
-        // onChange: (item) => {
-        //   setPopupSet({
-        //     message: "test",
-        //     icon: "/icon/warning.svg",
-        //     denyOnClick: () => handleCloseModal("popup"),
-        //     confirmOnClick: () => {
-        //       const memberData = hoistPreLoadData
-        //         .get()
-        //         [props.name].find((member) => `${member.id}` === item.value);
+        onChange: (item) => {
+          if (item.id === hoistFormik.get().values[props.name]) return;
+          setPopupSet({
+            message: "變更會員將清空收件人資料",
+            icon: "/icon/warning.svg",
+            denyText: "取消",
+            denyOnClick: () => {
+              hoistFormik.get().setValues(hoistFormik.get().values);
+              handleCloseModal("popup");
+            },
+            confirmOnClick: () => {
+              const memberData = hoistPreLoadData
+                .get()
+                [props.name].find((member) => `${member.id}` === item.value);
 
-        //       const data = {
-        //         ...hoistFormik.get().values,
-        //         [props.name]: item.value,
-        //         member_code: memberData.code,
-        //         payment: memberData.payment,
-        //         person_list: [
-        //           {
-        //             id: "_",
-        //             main_reciever: true,
-        //           },
-        //         ],
-        //       };
+              const data = {
+                ...hoistFormik.get().values,
+                [props.name]: item.value,
+                member_code: memberData.code,
+                payment: memberData.payment,
+                person_list: [
+                  {
+                    id: "_",
+                    main_reciever: true,
+                  },
+                ],
+              };
 
-        //       hoistFormik.get().setValues(data);
-        //       handleCloseModal("popup");
-        //     },
-        //   });
-        //   handleShowModal("popup");
-        // },
+              hoistFormik.get().setValues(data);
+              handleCloseModal("popup");
+            },
+          });
+          handleShowModal("popup");
+        },
       })}
       <ModalWrapper key="popup" show={isModalOpen("popup")} size="lg">
         <PopUp
@@ -53,6 +68,7 @@ const SaleMember = (props) => {
           title={popupSet.message}
           denyOnClick={popupSet.denyOnClick}
           confirmOnClick={popupSet.confirmOnClick}
+          denyText={popupSet.denyText}
         />
       </ModalWrapper>
     </>
