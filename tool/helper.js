a68f6f61d266bf2c0b1262fc55e48aa0cac54295
haohan <han.chang@i2csolution.com>
2024-08-13 15:24:11 +0800

    add check phone

--- tool/helper.js
+++ tool/helper.js
@@ -16,6 +16,8 @@ export const getFileUrl = (event) => {
   return file && URL.createObjectURL(file);
 };
 
+export const checkPhone = (phone) => /^(?!09)\d{8,10}$|^09\d{8}$/.test(phone)
+
 export const checkExpires = (time) =>
   time ? time * 1000 < Date.now() : console.log("Invalid exp.");
 
@@ -53,33 +55,13 @@ export const checkArray = (arr) => Array.isArray(arr) && arr.length > 0;
 
 export const toArray = (target) => (Array.isArray(target) ? target : [target]);
 
-export const getAddPadding = (() => {
-  const checkType = (target) => {
-    const type = typeof target;
-    if (type === "string") return true;
-    throw Error(`addPadding only accept string input but get "${type}" type.`);
-  };
-
-  return (symbol) => {
-    checkType(symbol);
-
-    return (str, digits) => {
-      checkType(str);
-      while (str.length < digits) str = symbol + str;
-      return str;
-    };
-  };
-})();
-
-export const addZeroPadding = getAddPadding("0");
-
 export const getCurrentTime = () => {
   const date = new Date();
-  const month = addZeroPadding(`${date.getMonth() + 1}`, 2);
-  const day = addZeroPadding(`${date.getDate()}`, 2);
-  const hour = addZeroPadding(`${date.getHours()}`, 2);
-  const minute = addZeroPadding(`${date.getMinutes()}`, 2);
-  const second = addZeroPadding(`${date.getSeconds()}`, 2);
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const day = `${date.getDate()}`.padStart(2, "0");
+  const hour = `${date.getHours()}`.padStart(2, "0");
+  const minute = `${date.getMinutes()}`.padStart(2, "0");
+  const second = `${date.getSeconds()}`.padStart(2, "0");
 
   return `${date.getFullYear()}-${month}-${day} ${hour}:${minute}:${second}`;
 };
